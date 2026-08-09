1c6a2b84e3d5cb3ca74448ed19f1d85728e91148
Aniket Biradar <biradaraniket6@gmail.com>
2026-08-09 12:57:43 +0530

    Added basic models and registered those models in admin.py

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from subjects.models import Subject, Topic
+from core.models import BaseModel
 # Create your models here.
+
+class Question(BaseModel):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    question_text = models.TextField()
+
+    option_a = models.TextField()
+    option_b = models.TextField()
+    option_c = models.TextField()
+    option_d = models.TextField()
+
+    correct_option = models.CharField(max_length=1)
+
+    explanation = models.TextField()
+
+    difficulty_level = models.IntegerField()
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_global = models.BooleanField(default=False)
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected')
+    ]
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return self.question_text[:50]
